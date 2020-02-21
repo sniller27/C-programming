81c184a313815a11afc46188ef365ca4c9caf30b
Jonas <sniller27@hotmail.com>
2020-02-21 17:59:14 +0100

    temporary header added. discount added

--- afl1.c
+++ afl1.c
@@ -1,6 +1,7 @@
 #include <stdio.h> /* printf, sprintf */
 #include <stdlib.h>
 // #include <locale.h>
+#define MAX 100 
 
 int main(int argc,char *argv[])
 {
@@ -8,14 +9,12 @@ int main(int argc,char *argv[])
 
     //variables
     FILE* fp = fopen("bestilling.txt","r"); 
-    char str1[10], str2[10], str3[10];
-    int val1, val2;
-
 
     // int price_reading = 0;
     float sum = 0;
-    int total_price = 0;
-    float price_of_products = 0;
+    int total_price = 0, product_qty;
+    float price_of_products, price_of_products_discount, product_price, product_price_discount;
+    char product_name[255];
 
     //check if file exists 
     if (fp==NULL) 
@@ -24,20 +23,52 @@ int main(int argc,char *argv[])
         return 0; 
     }
 
-    //read from file
-    int product_qty;
-    char product_name[255];
-    float product_price;
 
+    int readfile_counter = 0;
+    
+    //1. line variables
+    char product_qty_header[MAX], product_name_header[MAX], product_price_header[MAX];
+
+    //read from file
     //product_name peger vi på en memory adresse (og det gør product_qty automatisk)
     while(fscanf(fp, "%d %s %f", &product_qty, product_name, &product_price)!=EOF){
+        readfile_counter++;
 
+        if(readfile_counter == 1) {
+            //printf("hello: %-*d %-*s %-*f", 20, product_qty, product_name, product_price);
+            
+            // printf("%-*s", 20, gcvt(product_qty, 6, product_qty_header));
+            // printf("%-*s", 20, product_name);
+            // printf("%-*.2f", 20, product_price);
+
+            printf("%-*s", 20, "Varenavn");
+            printf("%-*s", 20, "Antal");
+            printf("%-*s", 20, "Stkpris");
+            printf("%-*s", 20, "Stkpris (m. rabat)");
+            printf("%-*s", 20, "Saldo");
+            printf("%-*s", 20, "Saldo (m. rabat)");
+
+            printf("\n");
+
+            // continue;
+        }
+
+        product_price_discount = product_price - (15.0/100.0)*product_price;
         price_of_products = product_qty*product_price;
+        price_of_products_discount = product_qty*product_price_discount;
         
-        printf("%-*d", 20, product_qty );
-        printf("%-*s", 20, product_name );
-        printf("%-*.2f", 20, product_price );
-        printf("%-*.2f", 20, product_qty*product_price );
+        //printing to output
+        printf("%-*s", 20, product_name);
+        printf("%-*d", 20, product_qty);
+        printf("%-*.2f", 20, product_price);
+        printf("%-*.2f", 20, product_price_discount);
+        printf("%-*.2f", 20, price_of_products);
+        printf("%-*.2f", 20, price_of_products_discount);
+
+        //calculations discount
+        if (product_qty > 10) {
+            price_of_products = price_of_products_discount;
+        }
 
         sum += price_of_products;
 
