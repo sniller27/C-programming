12bcd8197e787cc474a91a4ec125a1d0dea52ec1
Jonas <sniller27@hotmail.com>
2020-02-21 19:14:45 +0100

    writing to file by using fprintf and FILE struct from stdio

--- afl1.c
+++ afl1.c
@@ -8,7 +8,7 @@ int main(int argc,char *argv[])
     // setlocale(LC_ALL, "en_US.UTF-8");
 
     //variables
-    FILE* fp = fopen("bestilling.txt","r"); 
+    FILE* fp_order = fopen("bestilling.txt","r"); 
 
     // int price_reading = 0;
     float sum = 0;
@@ -17,12 +17,16 @@ int main(int argc,char *argv[])
     char product_name[255];
 
     //check if file exists 
-    if (fp==NULL) 
+    if (fp_order==NULL) 
     { 
         printf("no such file."); 
         return 0; 
     }
 
+    FILE* fp_invoice = fopen("faktura.txt", "w");
+    // fprintf(fp_invoice, "This is testing for fprintf...\n");
+    // fputs("This is testing for fputs...\n", fp_invoice);
+    
 
     int readfile_counter = 0;
     
@@ -31,7 +35,7 @@ int main(int argc,char *argv[])
 
     //read from file
     //product_name peger vi på en memory adresse (og det gør product_qty automatisk)
-    while(fscanf(fp, "%d %s %f", &product_qty, product_name, &product_price)!=EOF){
+    while(fscanf(fp_order, "%d %s %f", &product_qty, product_name, &product_price)!=EOF){
         readfile_counter++;
 
         if(readfile_counter == 1) {
@@ -41,13 +45,20 @@ int main(int argc,char *argv[])
             // printf("%-*s", 20, product_name);
             // printf("%-*.2f", 20, product_price);
 
+            fprintf(fp_invoice, "%-*s", 20, "Varenavn");
             printf("%-*s", 20, "Varenavn");
+            fprintf(fp_invoice, "%-*s", 20, "Antal");
             printf("%-*s", 20, "Antal");
+            fprintf(fp_invoice, "%-*s", 20, "Stkpris");
             printf("%-*s", 20, "Stkpris");
+            fprintf(fp_invoice, "%-*s", 20, "Stkpris (m. rabat)");
             printf("%-*s", 20, "Stkpris (m. rabat)");
+            fprintf(fp_invoice, "%-*s", 20, "Saldo");
             printf("%-*s", 20, "Saldo");
+            fprintf(fp_invoice, "%-*s", 20, "Saldo (m. rabat)");
             printf("%-*s", 20, "Saldo (m. rabat)");
 
+            fprintf(fp_invoice, "\n");
             printf("\n");
 
             // continue;
@@ -58,11 +69,17 @@ int main(int argc,char *argv[])
         price_of_products_discount = product_qty*product_price_discount;
         
         //printing to output
+        fprintf(fp_invoice, "%-*s", 20, product_name);
         printf("%-*s", 20, product_name);
+        fprintf(fp_invoice, "%-*d", 20, product_qty);
         printf("%-*d", 20, product_qty);
+        fprintf(fp_invoice, "%-*.2f", 20, product_price);
         printf("%-*.2f", 20, product_price);
+        fprintf(fp_invoice, "%-*.2f", 20, product_price_discount);
         printf("%-*.2f", 20, product_price_discount);
+        fprintf(fp_invoice, "%-*.2f", 20, price_of_products);
         printf("%-*.2f", 20, price_of_products);
+        fprintf(fp_invoice, "%-*.2f", 20, price_of_products_discount);
         printf("%-*.2f", 20, price_of_products_discount);
 
         //calculations discount
@@ -72,13 +89,18 @@ int main(int argc,char *argv[])
 
         sum += price_of_products;
 
+        fprintf(fp_invoice, "\n");
         printf("\n");        
     }  
 
+    fprintf(fp_invoice, "Total price: %.2f\n", sum);
     printf("Total price: %.2f\n", sum );
 
+    // fflush(fp_invoice);
+
     //close file connection
-    fclose(fp);
+    fclose(fp_order);
+    fclose(fp_invoice);
 
     //prevent prompt from closing
     // system("pause");
