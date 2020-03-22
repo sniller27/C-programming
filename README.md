009acbcaf052fe329551de0315eb9de7b18d9b26
Jonas <sniller27@hotmail.com>
2020-03-22 16:04:16 +0100

    project emptied

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 2. Semester C-programming
+Aflevering 2
---
+++ afl2.c
@@ -0,0 +1,9 @@
+#include <stdio.h> /* printf, sprintf */
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc,char *argv[])
+{
+    printf("hello world2!");
+    return 0;
+}
