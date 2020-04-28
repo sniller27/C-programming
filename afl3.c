92c6d540f2a6b10e7fc7dea77f033612adf15154
Jonas <sniller27@hotmail.com>
2020-04-28 12:47:09 +0200

    afl2 changed to basic setup. reading from file

---
+++ afl3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// internal constants
+#include "constants/my_constants.h"
+
+// internal structs
+#include "structs/my_structs.h"
+
+// internal libraries
+#include "functions/print_write_functions.h"
+#include "functions/my_functions.h"
+
+int main(int argc,char *argv[])
+{
+    //declaring and assigning array sizes (each file have 31 lines of data)
+    int TEMP_2011_1_SIZE, TEMP_2020_1_SIZE, RAIN_2011_1_SIZE, RAIN_2020_1_SIZE;
+    TEMP_2011_1_SIZE = TEMP_2020_1_SIZE = RAIN_2011_1_SIZE = RAIN_2020_1_SIZE = FIXED_ARRAY_SIZE;
+
+    //creating filepointer for writing outputs to file
+    FILE* output_file = fopen("output.txt", "w");
+
+    //assigning data arrays to structs (data stored in arrays)
+    struct Year y_2011, y_2020;
+    scan_values_from_file(TEMP_2011_1_SIZE, "temp_avg_2011_1", y_2011.temperature.date, y_2011.temperature.weather_value);
+
+    print_write_string("\n", output_file);
+
+    return 0;
+}
+
+
