c23528b10b989176738d5d05e92114c8aa5667f9
Jonas <sniller27@hotmail.com>
2020-05-10 20:03:50 +0200

    quick fix for task 7

--- functions/student_search.h
+++ functions/student_search.h
@@ -2,7 +2,7 @@ void search_students_lastname(int size, struct Student students[], char lastname
 
     for (int i = 0; i < size; i++)
     {
-        if(strcmp(students[i].lastname, "Hansen") == 0) {
+        if(strcmp(students[i].lastname, lastname) == 0) {
             print_student_data(1, &students[i]);
         }
     }
